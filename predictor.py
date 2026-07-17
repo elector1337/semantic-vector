2e08227b33e3c5b9bff6949caccdd56f77365e6d
Алексей Сысалов <alex@MacBook-Air--Usty.local>
2026-07-18 01:25:03 +0700

    aruco+dataset_gen

---
+++ predictor.py
@@ -0,0 +1,13 @@
+# class TrajectoryPredictor:
+
+#     def __init__(self, model_path):
+#         ...
+
+#     def predict(self, marker_id):
+
+#         history = history_manager.get_window(marker_id)
+
+#         if history is None:
+#             return None
+
+#         return model(history)
